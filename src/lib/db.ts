9bf978c7a51606356e67bfb0a482043622af2df9
bishalghale98 <bishalghle@gmail.com>
2026-01-27 10:42:30 +0545

    feat: Initialize and export Prisma client with PostgreSQL adapter for database connectivity.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,18 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient }
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient
+}
 
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({ log: ['query', 'info', 'warn'] })
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const prisma = globalForPrisma.prisma || new PrismaClient({
+  adapter,
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
-
