d484130b31761bbe134a0af14dc40d2f414bdf22
bishalghale98 <bishalghle@gmail.com>
2026-01-27 10:35:18 +0545

    feat: Initialize and export Prisma client for database interactions.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,10 +2,10 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient }
 
-const prisma =
+export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({ log: ['query', 'info', 'warn'] })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export default prisma
+
